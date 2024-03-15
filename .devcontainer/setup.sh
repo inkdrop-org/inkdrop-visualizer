633259271c506ddfb526c161988d15ca21db02a9
John Rowley <johnrowleyster@gmail.com>
2024-03-15 05:43:53 +0000

    chore: added devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+# here we install all the dependencies that chrome has.
+# https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#chrome-doesnt-launch-on-linux
+sudo apt update && sudo apt install -y ca-certificates fonts-liberation \
+    libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 \
+    libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 \
+    libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 \
+    libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 \
+    libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 \
+    libxtst6 lsb-release wget xdg-utils
+
+npm ci
