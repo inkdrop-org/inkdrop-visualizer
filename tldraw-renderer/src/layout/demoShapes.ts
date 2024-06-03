a4bf488372b365e675fa356ae0c8a3c9aa37c4c4
albschi <albertoschillaci30@gmail.com>
2024-06-03 16:13:35 +0200

    update demo tooltip shapes

--- tldraw-renderer/src/layout/demoShapes.ts
+++ tldraw-renderer/src/layout/demoShapes.ts
@@ -1,7 +1,7 @@
 export const demoShapes = [
     {
-        "x": 1982.5,
-        "y": 802.7775020854995,
+        "x": 3036.222462640292,
+        "y": -509.173528430375,
         "rotation": 0,
         "isLocked": false,
         "opacity": 1,
@@ -9,8 +9,8 @@ export const demoShapes = [
         "id": "shape:AKWTaSiXQ7JMxwCM9HMFn",
         "type": "geo",
         "props": {
-            "w": 430.00000000000034,
-            "h": 289.7244402689863,
+            "w": 679.6247878980514,
+            "h": 457.9160727132356,
             "geo": "rectangle",
             "color": "black",
             "labelColor": "black",
@@ -25,87 +25,12 @@ export const demoShapes = [
             "url": ""
         },
         "parentId": "page:page",
-        "index": "a0",
+        "index": "a9",
         "typeName": "shape"
     },
     {
-        "x": 1077.5,
-        "y": 802.7775020854995,
-        "rotation": 0,
-        "isLocked": false,
-        "opacity": 1,
-        "meta": {},
-        "id": "shape:tbi-EUy_TFz2_cuYrrhh2",
-        "type": "geo",
-        "props": {
-            "w": 789.9999999999999,
-            "h": 289.7244402689863,
-            "geo": "rectangle",
-            "color": "black",
-            "labelColor": "black",
-            "fill": "semi",
-            "dash": "solid",
-            "size": "m",
-            "font": "draw",
-            "text": "",
-            "align": "middle",
-            "verticalAlign": "middle",
-            "growY": 0,
-            "url": ""
-        },
-        "parentId": "page:page",
-        "index": "a6",
-        "typeName": "shape"
-    },
-    {
-        "x": 1118.3502781162572,
-        "y": 845.5815627000106,
-        "rotation": 0,
-        "isLocked": false,
-        "opacity": 1,
-        "meta": {},
-        "id": "shape:-q-PwczHd26zQX5wykAVp",
-        "type": "text",
-        "props": {
-            "color": "black",
-            "size": "m",
-            "w": 441.9082299999096,
-            "text": "Getting started:",
-            "font": "mono",
-            "align": "middle",
-            "autoSize": true,
-            "scale": 1.9096641944628099
-        },
-        "parentId": "page:page",
-        "index": "a7",
-        "typeName": "shape"
-    },
-    {
-        "x": 1118.3502781162572,
-        "y": 907.4516987503016,
-        "rotation": 0,
-        "isLocked": false,
-        "opacity": 1,
-        "meta": {},
-        "id": "shape:-E1K7m9pDp6oZQtbR_VXS",
-        "type": "text",
-        "props": {
-            "color": "black",
-            "size": "m",
-            "w": 698.2994437674856,
-            "text": "Zoom and pan using your mouse\nUse filters at the top left\nClick on a resource to see additional details",
-            "font": "sans",
-            "align": "start",
-            "autoSize": true,
-            "scale": 1.4635086747591146
-        },
-        "parentId": "page:page",
-        "index": "a8",
-        "typeName": "shape"
-    },
-    {
-        "x": 2025.782025220472,
-        "y": 845.5815627000104,
+        "x": 3104.6306887106657,
+        "y": -441.5207362994162,
         "rotation": 0,
         "isLocked": false,
         "opacity": 1,
@@ -120,15 +45,15 @@ export const demoShapes = [
             "font": "mono",
             "align": "middle",
             "autoSize": true,
-            "scale": 1.9096641944628099
+            "scale": 3.018267728182068
         },
         "parentId": "page:page",
         "index": "aA",
         "typeName": "shape"
     },
     {
-        "x": 2026.5251460890595,
-        "y": 907.4516987503018,
+        "x": 3105.8052081587985,
+        "y": -343.73357794964204,
         "rotation": 0,
         "isLocked": false,
         "opacity": 1,
@@ -143,15 +68,15 @@ export const demoShapes = [
             "font": "sans",
             "align": "start",
             "autoSize": true,
-            "scale": 1.4635086747591146
+            "scale": 2.3131087736514435
         },
         "parentId": "page:page",
         "index": "aB",
         "typeName": "shape"
     },
     {
-        "x": 2259.8358488464955,
-        "y": 910.7556703953803,
+        "x": 3445.1029583728982,
+        "y": -321.085354627804,
         "rotation": 0,
         "isLocked": false,
         "opacity": 1,
@@ -159,8 +84,8 @@ export const demoShapes = [
         "id": "shape:SM-b8UyFD3ZUqbJwYnYJi",
         "type": "geo",
         "props": {
-            "w": 44.13959226617101,
-            "h": 44.13959226617101,
+            "w": 44.58805662389258,
+            "h": 45.34835750330464,
             "geo": "rectangle",
             "color": "black",
             "labelColor": "black",
@@ -179,8 +104,8 @@ export const demoShapes = [
         "typeName": "shape"
     },
     {
-        "x": 2260.1339156708873,
-        "y": 955.6439740193302,
+        "x": 3445.1029583728982,
+        "y": -250.48463504314594,
         "rotation": 0,
         "isLocked": false,
         "opacity": 1,
@@ -188,8 +113,8 @@ export const demoShapes = [
         "id": "shape:g7CyezO1nc-5AR-FTwmu9",
         "type": "geo",
         "props": {
-            "w": 44.13959226617101,
-            "h": 44.13959226617101,
+            "w": 45.05915802460694,
+            "h": 45.05915802460694,
             "geo": "rectangle",
             "color": "black",
             "labelColor": "black",
@@ -208,8 +133,8 @@ export const demoShapes = [
         "typeName": "shape"
     },
     {
-        "x": 2259.7639660038108,
-        "y": 1000.687959363054,
+        "x": 3445.1029583728982,
+        "y": -179.29159089125972,
         "rotation": 0,
         "isLocked": false,
         "opacity": 1,
@@ -217,8 +142,8 @@ export const demoShapes = [
         "id": "shape:T2ibOUtHEsmkcPx_KtVAa",
         "type": "geo",
         "props": {
-            "w": 44.13959226617101,
-            "h": 44.13959226617101,
+            "w": 44.474444154792316,
+            "h": 44.474444154792316,
             "geo": "rectangle",
             "color": "black",
             "labelColor": "black",
@@ -234,6 +159,81 @@ export const demoShapes = [
         },
         "parentId": "page:page",
         "index": "aE",
+        "typeName": "shape"
+    },
+    {
+        "x": 1605.8493625292776,
+        "y": -509.173528430375,
+        "rotation": 0,
+        "isLocked": false,
+        "opacity": 1,
+        "meta": {},
+        "id": "shape:tbi-EUy_TFz2_cuYrrhh2",
+        "type": "geo",
+        "props": {
+            "w": 1248.612982417349,
+            "h": 457.9160727132356,
+            "geo": "rectangle",
+            "color": "black",
+            "labelColor": "black",
+            "fill": "semi",
+            "dash": "solid",
+            "size": "m",
+            "font": "draw",
+            "text": "",
+            "align": "middle",
+            "verticalAlign": "middle",
+            "growY": 0,
+            "url": ""
+        },
+        "parentId": "page:page",
+        "index": "aF",
+        "typeName": "shape"
+    },
+    {
+        "x": 1670.4141569486678,
+        "y": -441.52073629941526,
+        "rotation": 0,
+        "isLocked": false,
+        "opacity": 1,
+        "meta": {},
+        "id": "shape:-q-PwczHd26zQX5wykAVp",
+        "type": "text",
+        "props": {
+            "color": "black",
+            "size": "m",
+            "w": 441.9082299999096,
+            "text": "Getting started:",
+            "font": "mono",
+            "align": "middle",
+            "autoSize": true,
+            "scale": 3.018267728182068
+        },
+        "parentId": "page:page",
+        "index": "aG",
+        "typeName": "shape"
+    },
+    {
+        "x": 1670.4141569486678,
+        "y": -343.73357794964204,
+        "rotation": 0,
+        "isLocked": false,
+        "opacity": 1,
+        "meta": {},
+        "id": "shape:-E1K7m9pDp6oZQtbR_VXS",
+        "type": "text",
+        "props": {
+            "color": "black",
+            "size": "m",
+            "w": 698.2994437674856,
+            "text": "Zoom and pan using your mouse\nUse filters at the top left\nClick on a resource to see additional details",
+            "font": "sans",
+            "align": "start",
+            "autoSize": true,
+            "scale": 2.3131087736514435
+        },
+        "parentId": "page:page",
+        "index": "aH",
         "typeName": "shape"
     }
 ]
